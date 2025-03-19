664bdb35bcb420c6c5aee47281f8b8f44a89b30a
Oleksandr Korlyhanov <sashakorluxan@gmail.com>
2025-03-19 20:12:25 +0200

    updated fonts, upgraded startPage

--- client/src/view/start/StartGamePage.jsx
+++ client/src/view/start/StartGamePage.jsx
@@ -7,41 +7,70 @@ export function StartGamePage() {
   };
   return (
     <div className="start_container">
-      <h1>Обрати режим</h1>
+      <h2>Обрати режим</h2>
       <main>
         <div className="container_element">
-          <div className="image">image</div>
-          <div className="text"> text</div>
+          <img className="image" src="/images/create_game.png" alt="Create" />
+          <div className="text">
+            <h3>Створити Гру</h3>
+            <ul>
+              <li className="bc2">Я заглушка 1</li>
+              <li className="bc2">
+                Я довга заглушка шоб побачити як буде виглядати
+              </li>
+              <li className="bc2">Я заглушка 3</li>
+            </ul>
+          </div>
           <div className="button_container">
             <Button
               variant="primary"
               size="large"
               onClick={() => handleClick("create")}
             >
-              Create Game
+              Створити Гру
             </Button>
           </div>
         </div>
         <div className="container_element">
-          <div className="image">image</div>
-          <div className="text"> text</div>
+          <img className="image" src="/images/join_game.png" alt="Join" />
+          <div className="text">
+            {" "}
+            <h3>Приєднатися До Гри</h3>
+            <ul>
+              <li className="bc2">Я заглушка 1</li>
+              <li className="bc2">
+                Я довга заглушка шоб побачити як буде виглядати
+              </li>
+              <li className="bc2">Я заглушка 3</li>
+            </ul>
+          </div>
           <div className="join_button_container">
             <input type="text" placeholder="Введіть код"></input>
             <Button variant="primary" size="small" disabled>
-              Join
+              {`>`}
             </Button>
           </div>
         </div>
         <div className="container_element">
-          <div className="image">image</div>
-          <div className="text"> text</div>
+          <img className="image" src="/images/browse_list.png" alt="Browse" />
+          <div className="text">
+            {" "}
+            <h3>Список Ігрових Сесій</h3>
+            <ul>
+              <li className="bc2">Я заглушка 1</li>
+              <li className="bc2">
+                Я довга заглушка шоб побачити як буде виглядати
+              </li>
+              <li className="bc2">Я заглушка 3</li>
+            </ul>
+          </div>
           <div className="button_container">
             <Button
               variant="primary"
               size="large"
               onClick={() => handleClick("list")}
             >
-              Browse List
+              Переглянути Список
             </Button>
           </div>
         </div>
